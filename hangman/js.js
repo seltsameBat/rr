4dedf17d93c370c531f91ec17412b37591ac4b43
seltsameBat <152424723+seltsameBat@users.noreply.github.com>
2025-02-03 08:46:36 -0800

    Update js.js

--- hangman/js.js
+++ hangman/js.js
@@ -2,7 +2,7 @@ window.onload = function () {
 
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z',' '];
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
   
   var categories;         // Array of topics
   var chosenCategory;     // Selected catagory
